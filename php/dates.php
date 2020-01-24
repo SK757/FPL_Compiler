bff9adb60abf8fe3942214f571316812721ceae1
Sean Kirk <seankirk757@gmail.com>
2020-01-24 23:39:18 +0000

    Added new deadline dates

--- php/dates.php
+++ php/dates.php
@@ -15,6 +15,12 @@ if ($date >= '2020-01-21 19:15:00' && $date < '2020-02-01 12:15:00') {
     $jpicks = file_get_contents("https://fantasy.premierleague.com/api/entry/581004/event/27/picks/");
 } elseif ($date >= '2020-02-28 19:45:00' && $date < '2020-03-07 12:15:00') {
     $jpicks = file_get_contents("https://fantasy.premierleague.com/api/entry/581004/event/28/picks/");
+} elseif ($date >= '2020-03-07 12:15:00' && $date < '2020-03-14 12:15:00') {
+    $jpicks = file_get_contents("https://fantasy.premierleague.com/api/entry/581004/event/29/picks/");
+} elseif ($date >= '2020-03-14 12:15:00' && $date < '2020-03-20 19:45:00') {
+    $jpicks = file_get_contents("https://fantasy.premierleague.com/api/entry/581004/event/30/picks/");
+} elseif ($date >= '2020-03-20 19:45:00' && $date < '2020-04-04 12:15:00') {
+    $jpicks = file_get_contents("https://fantasy.premierleague.com/api/entry/581004/event/31/picks/");
 }
 
 
@@ -28,6 +34,12 @@ if ($date >= '2020-01-21 19:15:00' && $date < '2020-02-01 12:15:00') {
     $jlive = file_get_contents("https://fantasy.premierleague.com/api/event/27/live/");
 } elseif ($date >= '2020-02-28 19:45:00' && $date < '2020-03-07 12:15:00') {
     $jlive = file_get_contents("https://fantasy.premierleague.com/api/event/28/live/");
+} elseif ($date >= '2020-03-07 12:15:00' && $date < '2020-03-14 12:15:00') {
+    $jlive = file_get_contents("https://fantasy.premierleague.com/api/event/29/live/");
+} elseif ($date >= '2020-03-14 12:15:00' && $date < '2020-03-20 19:45:00') {
+    $jlive = file_get_contents("https://fantasy.premierleague.com/api/event/30/live/");
+} elseif ($date >= '2020-03-20 19:45:00' && $date < '2020-04-04 12:15:00') {
+    $jlive = file_get_contents("https://fantasy.premierleague.com/api/event/31/live/");
 }
 
 $picks = json_decode($jpicks, true);
