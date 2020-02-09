dbe65a8c523092a5ad9a29e8959257d3ef368806
Sean Kirk <seankirk757@gmail.com>
2020-02-09 15:40:24 +0000

    Team page now returns player point quicker using live points rather than event points. Also changed key names to improve consistency

--- php/teamPoints.php
+++ php/teamPoints.php
@@ -16,60 +16,60 @@
 		        <tbody>
 		        	<?PHP
 		        	$count = 0; 
-		        	foreach($picks['picks'] as $key=>$item2) {
+		        	foreach($picks['picks'] as $key=>$item) {
 						foreach($live['elements'] as $key=>$item1) {
-							foreach($data['elements'] as $key=>$item) {
-								if ($item2['element'] === $item1['id'] && $item2['element'] === $item['id']) {
+							foreach($data['elements'] as $key=>$item2) {
+								if ($item['element'] === $item1['id'] && $item['element'] === $item2['id']) {
 					?><tr>
-						<td style="display: none;"><?PHP echo $item['chance_of_playing_next_round']; ?></td>
+						<td style="display: none;"><?PHP echo $item2['chance_of_playing_next_round']; ?></td>
 						<td class="deffo"><?PHP
-						if ($item['web_name'] === 'Alexander-Arnold') {
+						if ($item2['web_name'] === 'Alexander-Arnold') {
 							echo 'Trent';
 						} else {
-							echo $item['web_name'];
+							echo $item2['web_name'];
 						} ?></td>
 		                <?PHP echo '<td id="player_'.$count.'">';
-		                if($item2['multiplier'] === 2) {
-		                	echo $item['event_points'] * 2 . ' (c)';
-		                } elseif($item2['multiplier'] === 3) {
-		                	echo $item['event_points'] * 3 . ' (tc)';
+		                if($item['multiplier'] === 2) {
+		                	echo $item1['stats']['total_points'] * 2 . ' (c)';
+		                } elseif($item['multiplier'] === 3) {
+		                	echo $item1['stats']['total_points'] * 3 . ' (tc)';
 		                } else {
-			        		echo $item['event_points'];
+			        		echo $item1['stats']['total_points'];
 			        	}?></td>
 			        	<td><?PHP echo $item1['stats']['goals_scored']; ?>/<?PHP echo $item1['stats']['assists']; ?></td>
-						<td><?PHP echo $item['ep_next']; ?></td>
-						<td><?PHP echo $item['goals_scored']; ?>/<?PHP echo $item['assists']; ?>/<?PHP echo $item['clean_sheets']; ?></td>
+						<td><?PHP echo $item2['ep_next']; ?></td>
+						<td><?PHP echo $item2['goals_scored']; ?>/<?PHP echo $item2['assists']; ?>/<?PHP echo $item2['clean_sheets']; ?></td>
 						<?PHP
 						// Yellow Card Ban #1
 						if ($date < '2019-12-28') {
-							if ($item['yellow_cards'] === 4) {
-								echo '<td style="background: #ffab1b;">' . $item['yellow_cards'] . '</td>';
-							} elseif ($item['yellow_cards'] > 4) {
-								echo '<td style="background: #c0020d; color: #fff;">' . $item['yellow_cards'] . '</td>';
+							if ($item2['yellow_cards'] === 4) {
+								echo '<td style="background: #ffab1b;">' . $item2['yellow_cards'] . '</td>';
+							} elseif ($item2['yellow_cards'] > 4) {
+								echo '<td style="background: #c0020d; color: #fff;">' . $item2['yellow_cards'] . '</td>';
 							} else {
-								echo '<td>' . $item['yellow_cards'] . '</td>';
+								echo '<td>' . $item2['yellow_cards'] . '</td>';
 							}
 						// Yellow Card Ban #2
 						} elseif ($date >= '2019-12-28' && $date < '2020-04-10') {
-							if ($item['yellow_cards'] === 8) {
-								echo '<td style="background: #ffe65b;">' . $item['yellow_cards'] . '</td>';
-							} elseif ($item['yellow_cards'] === 9) {
-								echo '<td style="background: #ffab1b;">' . $item['yellow_cards'] . '</td>';
-							} elseif ($item['yellow_cards'] > 9) {
-								echo '<td style="background: #c0020d; color: #fff;">' . $item['yellow_cards'] . '</td>';
+							if ($item2['yellow_cards'] === 8) {
+								echo '<td style="background: #ffe65b;">' . $item2['yellow_cards'] . '</td>';
+							} elseif ($item2['yellow_cards'] === 9) {
+								echo '<td style="background: #ffab1b;">' . $item2['yellow_cards'] . '</td>';
+							} elseif ($item2['yellow_cards'] > 9) {
+								echo '<td style="background: #c0020d; color: #fff;">' . $item2['yellow_cards'] . '</td>';
 							} else {
-								echo '<td>' . $item['yellow_cards'] . '</td>';
+								echo '<td>' . $item2['yellow_cards'] . '</td>';
 							}
 						// Yellow Card Ban #3
 						} elseif ($date >= '2020-04-10') {
-							if ($item['yellow_cards'] === 13) {
-								echo '<td style="background: #ffe65b;">' . $item['yellow_cards'] . '</td>';
-							} elseif ($item['yellow_cards'] === 14) {
-								echo '<td style="background: #ffab1b;">' . $item['yellow_cards'] . '</td>';
-							} elseif ($item['yellow_cards'] > 14) {
-								echo '<td style="background: #c0020d; color: #fff;">' . $item['yellow_cards'] . '</td>';
+							if ($item2['yellow_cards'] === 13) {
+								echo '<td style="background: #ffe65b;">' . $item2['yellow_cards'] . '</td>';
+							} elseif ($item2['yellow_cards'] === 14) {
+								echo '<td style="background: #ffab1b;">' . $item2['yellow_cards'] . '</td>';
+							} elseif ($item2['yellow_cards'] > 14) {
+								echo '<td style="background: #c0020d; color: #fff;">' . $item2['yellow_cards'] . '</td>';
 							} else {
-								echo '<td>' . $item['yellow_cards'] . '</td>';
+								echo '<td>' . $item2['yellow_cards'] . '</td>';
 							}
 						} ?>
 						 
