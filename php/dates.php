17de12f205496759ce3fca848f1787fdbd82a67c
Sean Kirk <seankirk757@gmail.com>
2020-08-13 14:08:27 +0100

    Further optimisation of dates file

--- php/dates.php
+++ php/dates.php
@@ -5,9 +5,6 @@ $date = date("Y-m-d H:i:s");
 $data = json_decode(file_get_contents("https://fantasy.premierleague.com/api/bootstrap-static/"), true);
 $leagues = json_decode(file_get_contents("https://fantasy.premierleague.com/api/entry/581004/"), true);
 
-$jpicks = file_get_contents("https://fantasy.premierleague.com/api/entry/581004/event/".$leagues['current_event']."/picks/");
-$jlive = file_get_contents("https://fantasy.premierleague.com/api/event/".$leagues['current_event']."/live/");
-
-$picks = json_decode($jpicks, true);
-$live = json_decode($jlive, true);
+$picks = json_decode(file_get_contents("https://fantasy.premierleague.com/api/entry/581004/event/".$leagues['current_event']."/picks/"), true);
+$live = json_decode(file_get_contents("https://fantasy.premierleague.com/api/event/".$leagues['current_event']."/live/"), true);
 ?>
