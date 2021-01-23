34a6b414ad2c00fc5f989990a3f6f1cb71ec6709
Sean Kirk <seankirk757@gmail.com>
2021-01-23 02:12:50 +0000

    Overhauled site into a Progressive Web App and upgraded icons to make use of the maskable icon format

--- team.php
+++ team.php
@@ -9,10 +9,10 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="styles/css/main.css?0.93">
     <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.14.0/css/all.css" crossorigin="anonymous" SameSite="none Secure">
-    <link rel="manifest" href="/favicon/manifest.json">
-    <link rel="apple-touch-icon" href="/favicon/maskable_icon.png">
-    <link rel="shortcut icon" href="/favicon/favicon.ico?v=0.2">
-    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg?v=0.2" color="#00e187">
+    <link rel="manifest" href="/favicon/manifest.json?v=0.51">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="shortcut icon" href="/favicon/favicon.ico?v=0.4">
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg?v=0.3" color="#37003c">
     <script src="js/sortable.js?=0.02"></script>
     <style>
         #table_cont {
