949502e2b997a1cacd176c20c1cc573044209801
Sean Kirk <seankirk757@gmail.com>
2021-03-31 00:24:43 +0100

    When user is offline, a page now displays indicating that they must go online to access current information

--- sw.js
+++ sw.js
@@ -1,38 +1,44 @@
+const CACHE_NAME = "v1";
+const urlsToCache = [ 'index.php', '/p', '/team', '/history', '/strength', '/offline.html' ];
+
+const self = this;
+
+//install sw
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('cache').then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.php",
-        "/p",
-        "/team",
-        "/history",
-        "/strength",
-        "/php/dates.php",
-        "/php/points.php",
-        "/php/teamPoints.php",
-        "/styles/css/main.css?=0.98",
-        "/styles/css/p.css?=0.9",
-        "/js/javascript.js?=0.89",
-        "/js/change.js?=1.1",
-        "/js/sortable.js?=0.02",
-        "/js/strength.js?=0.89",
-        "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(async function() {
-    try {
-      var res = await fetch(event.request);
-      var cache = await caches.open('cache');
-      cache.put(event.request.url, res.clone());
-      return res;
-    }
-    catch(error) {
-      return caches.match(event.request);
-    }
-  }());
+//listen for requests
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+     .then(() => {
+      return fetch(event.request)
+        .catch(() => caches.match('offline.html'));
+     })
+  );
+});
+
+// activate sw
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => Promise.all(
+      cacheNames.map((cacheName) => {
+        if(!cacheWhitelist.includes(cacheName)) {
+          return caches.delete(cacheName);
+        }
+      })
+    ))
+  );
+
 });
