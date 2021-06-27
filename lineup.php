2ed50062ffeaaf01b72bbea0cb30003d11bd9e3f
Sean Kirk <seankirk757@gmail.com>
2021-06-27 19:15:31 +0100

    Added Gameweek update notification

--- lineup.php
+++ lineup.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5">
     <meta name="Description" content="FPL Lineup">
     <meta name="theme-color" content="#37003c">
-    <link rel="stylesheet" type="text/css" href="styles/css/lineup.css?0.1">
+    <link rel="stylesheet" type="text/css" href="styles/css/lineup.css?=0.3">
     <link rel="manifest" href="/favicon/manifest.json?=0.52">
     <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
     <link rel="shortcut icon" href="/favicon/favicon.ico?=0.4">
@@ -14,21 +14,24 @@
 </head>
 <body>
     <main class="grid-container">
-        <section class="flex-container">
+        
+        <?PHP include 'php/lineup/check.php'; ?>
+        
+        <section class="flex-container gk">
             <div>
                 <b><p id="player_1"><?PHP include 'php/lineup/goalkeeper.php';?></p></b>
             </div>
         </section>
 
-        <section class="flex-container">
+        <section class="flex-container def">
             <?PHP include 'php/lineup/defender.php';?>
         </section>
 
-        <section class="flex-container">
+        <section class="flex-container mid">
             <?PHP include 'php/lineup/midfield.php';?>
         </section>
 
-        <section class="flex-container">
+        <section class="flex-container fwd">
             <?PHP include 'php/lineup/forward.php';?>
         </section>
     </main>
---
+++ php/lineup/check.php
@@ -0,0 +1,19 @@
+<?PHP
+include 'php/dates.php';
+if (is_array($picks)) {
+	foreach($picks['picks'] as $key=>$item) {
+		break;
+	}
+} else {
+	echo "<h1 style='grid-area:gk/gk/fwd/fwd;display:flex;justify-content:center;align-items:center;'>Gameweek Is Being Updated</h1>"
+?>
+<style type="text/css">
+	section{
+		display:none !important;
+	}
+	.grid-container {
+		height: calc(100vh - 12px);
+	}
+</style>
+<?php
+} ?>
