f3d0cc919de311d5c66359a734db0cd79e82a649
Sean Kirk <seankirk757@gmail.com>
2021-08-20 00:21:27 +0100

    Added captaincy visual to lineup page

--- php/lineup/forward.php
+++ php/lineup/forward.php
@@ -6,7 +6,14 @@ foreach($picks['picks'] as $key=>$item) {
 	    foreach($data['elements'] as $key=>$item2) {
 	        if ($item['element'] === $item1['id'] && $item['element'] === $item2['id']) {
 	        	if($item['position'] === 9 && $item2['element_type'] == 4 || $item['position'] === 10 && $item2['element_type'] == 4 || $item['position'] === 11) {
-	        		echo '<div class="column"><div class="image" style="background-image: url(https://resources.premierleague.com/premierleague/photos/players/110x140/p' . $item2['code'] . '.png);"></div><div class="player"><p>' . $item2['web_name'] . '</p></div><div class="points"><b><p>' . $item1['stats']['total_points'] . '</p></b></div></div>';
+	        		echo '<div class="column"><div class="image" style="background-image: url(https://resources.premierleague.com/premierleague/photos/players/110x140/p' . $item2['code'] . '.png);"></div><div class="player"><p>' . $item2['web_name'] . '</p></div><div class="points"><b><p>';
+	        		if($item['multiplier'] === 2) {
+	                	echo $item1['stats']['total_points'] * 2 . ' (c)</p></b></div></div>';
+	                } elseif($item['multiplier'] === 3) {
+	                	echo $item1['stats']['total_points'] * 3 . ' (tc)</p></b></div></div>';
+	                } else {
+		        		echo $item1['stats']['total_points'] . '</p></b></div></div>';
+		        	}
 	        	}
 	        	++$count;
 	        }
--- php/lineup/goalkeeper.php
+++ php/lineup/goalkeeper.php
@@ -5,7 +5,14 @@ foreach($picks['picks'] as $key=>$item) {
 	    foreach($data['elements'] as $key=>$item2) {
 	        if ($item['element'] === $item1['id'] && $item['element'] === $item2['id']) {
 	        	if($item['position'] === 1) {
-	        		echo '<div class="image" style="background-image: url(https://resources.premierleague.com/premierleague/photos/players/110x140/p' . $item2['code'] . '.png);"></div><div class="player"><p>' . $item2['web_name'] . '</p></div><div class="points"><b><p>' . $item1['stats']['total_points'] . '</p></b></div>';
+	        		echo '<div class="image" style="background-image: url(https://resources.premierleague.com/premierleague/photos/players/110x140/p' . $item2['code'] . '.png);"></div><div class="player"><p>' . $item2['web_name'] . '</p></div><div class="points"><b><p>';
+	        		if($item['multiplier'] === 2) {
+	                	echo $item1['stats']['total_points'] * 2 . ' (c)</p></b></div>';
+	                } elseif($item['multiplier'] === 3) {
+	                	echo $item1['stats']['total_points'] * 3 . ' (tc)</p></b></div>';
+	                } else {
+		        		echo $item1['stats']['total_points'] . '</p></b></div>';
+		        	}
 	        	}
 	        }
 	    }
