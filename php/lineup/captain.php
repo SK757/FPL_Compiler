8bd62f2d5a23fdae330c1a88b502146b15076d36
Sean Kirk <seankirk757@gmail.com>
2021-12-10 00:58:23 +0000

    Captain indicator changed from text to icon. Set up for player info modal on click

--- php/lineup/captain.php
+++ php/lineup/captain.php
@@ -1,13 +1,13 @@
 <?PHP
-echo '<div class="column"><div class="image" style="background-image: url(https://resources.premierleague.com/premierleague/photos/players/110x140/p' . $item2['code'] . '.png);"></div><div class="player"><b><p>' . $item2['web_name'];
+echo '<div class="column"><div class="image" style="background-image: url(https://resources.premierleague.com/premierleague/photos/players/110x140/p' . $item2['code'] . '.png);">';
 if($item['multiplier'] === 2) {
-	echo ' (c)';
+	echo '<span class="capt">C</span>';
 } elseif($item['multiplier'] === 3) {
-	echo ' (tc)';
+	echo '<span class="capt">TC</span>';
 } elseif($item['is_vice_captain'] === true) {
-	echo ' (vc)';
+	echo '<span class="capt">V</span>';
 }
-echo '</p></b></div><div class="points"><b><p>';
+echo '</div><div class="player"><b><p>' . $item2['web_name']. '</p></b></div><div class="points"><b><p>';
 if($item['multiplier'] === 2) {
 	echo $item1['stats']['total_points'] * 2;
 } elseif($item['multiplier'] === 3) {
--- php/lineup/midfield.php
+++ php/lineup/midfield.php
@@ -6,7 +6,13 @@ foreach($picks['picks'] as $key=>$item) {
 	        if ($item['element'] === $item1['id'] && $item['element'] === $item2['id']) {
 	        	if($item['position'] < 12 && $item2['element_type'] == 3) {
 	        		include 'captain.php';
-		        	echo '</p></b></div></div>';
+		        	echo '</p></b></div>';
+		        	?>
+
+		        	<!-- <button id="123" class="player_btn"></button> -->
+
+		        	<?PHP
+		        	echo '</div>';
 	        	}
 	        }
 	    }
