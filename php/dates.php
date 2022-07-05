4e37446a33e46c2a00e26d1a16e7bb1ce3b96721
Sean Kirk <seankirk757@gmail.com>
2022-07-05 21:17:36 +0100

    Changed team id

--- php/dates.php
+++ php/dates.php
@@ -3,10 +3,10 @@ date_default_timezone_set('Europe/London');
 $date = date("Y-m-d H:i:s");
 
 $data = json_decode(file_get_contents("https://fantasy.premierleague.com/api/bootstrap-static/"), true);
-$leagues = json_decode(file_get_contents("https://fantasy.premierleague.com/api/entry/635855/"), true);
+$leagues = json_decode(file_get_contents("https://fantasy.premierleague.com/api/entry/329312/"), true);
 $standings = json_decode(file_get_contents("https://fantasy.premierleague.com/api/leagues-classic/114089/standings/?page_new_entries=1&page_standings=1&phase=1"), true);
 
-$picks = json_decode(file_get_contents("https://fantasy.premierleague.com/api/entry/635855/event/".$leagues['current_event']."/picks/"), true);
+$picks = json_decode(file_get_contents("https://fantasy.premierleague.com/api/entry/329312/event/".$leagues['current_event']."/picks/"), true);
 $live = json_decode(file_get_contents("https://fantasy.premierleague.com/api/event/".$leagues['current_event']."/live/"), true);
 
 $nextgw = $leagues['current_event']+1;
