24a90ca34365c303b07bfc9e95daa2fdeef5f80e
Sean Kirk <seankirk757@gmail.com>
2022-07-06 14:45:53 +0100

    Changed main theme dark

--- strength.php
+++ strength.php
@@ -5,9 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5">
     <meta name="Description" content="FPL Team Strength">
-    <meta name="theme-color" media="(prefers-color-scheme: light)" content="#ffffff">
+    <meta name="theme-color" media="(prefers-color-scheme: light)" content="#1f1f1f">
     <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#1f1f1f">
-    <link rel="stylesheet" type="text/css" href="styles/css/main.css?=1.1">
+    <link rel="stylesheet" type="text/css" href="styles/css/main.css?=1.2">
     <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.14.0/css/all.css" crossorigin="anonymous" SameSite="none Secure">
     <link rel="manifest" href="/manifest.json">
     <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
