100c4e48dc5a6d9e32078b8bb1284d70e95fecbd
Sean Kirk <seankirk757@gmail.com>
2022-10-04 01:17:44 +0100

    Corrected yellow card suspension rule and reduced and simplified code

--- php/teamPoints.php
+++ php/teamPoints.php
@@ -54,27 +54,15 @@
 						<td><?PHP echo $item2['goals_scored']; ?>/<?PHP echo $item2['assists']; ?>/<?PHP echo $item2['clean_sheets']; ?></td>
 						<?PHP
 						// Yellow Card Ban #1
-						if ($leagues['current_event'] <= 19) {
-							if ($item2['yellow_cards'] === 4) {
-								echo '<td style="background: #ffab1b;">' . $item2['yellow_cards'] . '</td>';
-							} else {
-								echo '<td>' . $item2['yellow_cards'] . '</td>';
-							}
+						$gamesPlayed = $item2['total_points'] / $item2['points_per_game'];
+						if (round($gamesPlayed) <= 19 && $item2['yellow_cards'] === 4) {
+							echo '<td style="background: #ffab1b;">' . $item2['yellow_cards'] . '</td>';
+							
 						} // Yellow Card Ban #2
-						elseif ($leagues['current_event'] <= 33) {
-							if ($item2['yellow_cards'] === 9) {
+						elseif (round($gamesPlayed) >= 19 && round($gamesPlayed) <= 38 && $item2['yellow_cards'] === 9) {
 								echo '<td style="background: #ffab1b;">' . $item2['yellow_cards'] . '</td>';
-							} else {
-								echo '<td>' . $item2['yellow_cards'] . '</td>';
-							}
-						} // Yellow Card Ban #3
-						elseif ($leagues['current_event'] <= 38) {
-							if ($item2['yellow_cards'] === 14) {
-								echo '<td style="background: #ffab1b;">' . $item2['yellow_cards'] . '</td>';
-							} else {
-								echo '<td>' . $item2['yellow_cards'] . '</td>';
-							}
-						} elseif ($leagues['current_event'] >= 38) {
+						}
+						else {
 							echo '<td>' . $item2['yellow_cards'] . '</td>';
 						} ?>
 						
