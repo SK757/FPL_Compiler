70cd6f62d9f77ee37a29ab4e964a1a394abeb5d4
Sean Kirk <seankirk757@gmail.com>
2022-10-11 23:56:31 +0100

    Added loading screen on Lineup page

--- lineup.php
+++ lineup.php
@@ -8,14 +8,20 @@
     <meta name="theme-color" media="(prefers-color-scheme: light)" content="#37003c">
     <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#1f1f1f">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="styles/css/lineup.css?=0.87">
+    <link rel="stylesheet" type="text/css" href="styles/css/lineup.css?=0.88">
     <link rel="manifest" href="/manifest.json">
     <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
     <link rel="shortcut icon" href="/favicon/favicon.ico?=0.4">
     <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg?=0.3" color="#37003c">
 </head>
 <body>
-    <main class="grid-container">
+    <div class="loadWrapper">
+        <div class="loading">
+            <span class="loading__anim"></span>
+        </div>
+    </div>
+    
+    <main style="display: none;" class="grid-container">
         
         <?PHP include 'php/lineup/check.php'; ?>
         
@@ -36,7 +42,7 @@
             <?PHP include 'php/lineup/forward.php';?>
         </section>
     </main>
-    <section class="bench-container">
+    <section style="display: none;" class="bench-container">
         <section class="flex-container">
             <?PHP include 'php/lineup/subs.php';?>
         </section>
@@ -48,6 +54,12 @@
                 navigator.serviceWorker.register('/sw.js');
             });
         }
+
+        $(document).ready(function () {
+            $(".loadWrapper").hide();
+            $("main").show();
+            $("section").show();
+        });
 
         if (document.getElementById("chip").innerText === '(bboost)') {
             $(".bench .image").removeClass("bench-image"); 
