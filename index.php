a45ef1862130509776bacc5f552c997f3588cea5
Sean Kirk <seankirk757@gmail.com>
2022-12-28 19:01:01 +0000

    Set minimum width of points box

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="Description" content="FPL Hub">
     <meta name="theme-color" content="#02efff">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="styles/css/hub.css?=0.112">
+    <link rel="stylesheet" type="text/css" href="styles/css/hub.css?=0.114">
     <link rel="manifest" href="/manifest.json">
     <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
     <link rel="shortcut icon" href="/favicon/favicon.ico?=0.4">
@@ -31,6 +31,15 @@
 		    } ?>
 		    <span class="gwb">
 		    	<span>Gameweek <?php echo $leagues['current_event']; ?></span>
+		    	<?php 
+		    	$numItems = count($fixtures);
+		    	$i = 0;
+		    	foreach($fixtures as $key=>$fixture) {
+		    		if(++$i === $numItems && $fixture['finished_provisional'] == false) {
+                        echo '&nbsp;<p class="live">Live</p>';
+		    		}
+		    	}
+		    	?>
 		    </span>
 			<a href="team" target="_top"><h2 class="score"></h2><p>Points &rarr;</p></a>
 			<span class="gwdb">
