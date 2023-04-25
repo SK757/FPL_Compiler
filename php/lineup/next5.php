39fdbea014b6aca1a64065db080db493480e32d4
Sean Kirk <seankirk757@gmail.com>
2023-04-25 14:29:57 +0100

    Made GW bold instead of fixture

--- php/lineup/next5.php
+++ php/lineup/next5.php
@@ -9,13 +9,13 @@
                     if ($item2['team'] === $next5['team_h']) {
                         // get away team name
                         if ($next5['team_a'] === $teams['id']) {
-                            echo '<span class="diff'.$next5['team_h_difficulty'].'">'.$next5['event'].'<br><b>'.$teams['short_name'].'</b></span>';
+                            echo '<span class="diff'.$next5['team_h_difficulty'].'"><b>'.$next5['event'].'</b><br>'.$teams['short_name'].'</span>';
                         }
                     // if players team id equals fixtures away team
                     } elseif ($item2['team'] === $next5['team_a']) {
                         // get home team name
                         if ($next5['team_h'] === $teams['id']) {
-                            echo '<span style="text-transform: lowercase;" class="diff'.$next5['team_a_difficulty'].'">'.$next5['event'].'<br><b>'.$teams['short_name'].'</b></span>';
+                            echo '<span style="text-transform: lowercase;" class="diff'.$next5['team_a_difficulty'].'"><b>'.$next5['event'].'</b><br>'.$teams['short_name'].'</span>';
                         }
                     }
                 }
