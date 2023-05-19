bd3f8ebf1330e42d707431a53a5b88bd6048df96
Sean Kirk <seankirk757@gmail.com>
2023-05-19 23:16:08 +0100

    Made sure 'Did not play' doesn't show if player only plays 1 game in DGW

--- php/lineup/stpPoints.php
+++ php/lineup/stpPoints.php
@@ -1,8 +1,9 @@
 <?php
-$played = false;
-$notPlayed = false;
+$gamePlayed = false;
+$gameNotPlayed = false;
 $fix = 0;
 $stp = 0;
+$didPlay = false;
 $didNotPlay = false;
 foreach($item1['explain'] as $key=>$explain) {	
     foreach($fixtures as $key=>$fixture) {
@@ -11,20 +12,23 @@ foreach($item1['explain'] as $key=>$explain) {
                 //players fixture         //fixture info
             if ($explain['fixture'] === $fixture['id']) {
             	if ($fixture['started'] === true) {
-                    $played = true;
+                    $gamePlayed = true;
+                }
+                if ($fixture['finished_provisional'] === true && $item1['stats']['minutes'] > 0) {
+                    $didPlay = true;
                 }
                 if ($fixture['finished_provisional'] === true && $item1['stats']['minutes'] === 0) {
                     $didNotPlay = true;
                 }
                 if ($fixture['started'] === false) {
-                    $notPlayed = true;
+                    $gameNotPlayed = true;
                     ++$stp;
                 }
             }
         }
     }++$fix;
 }
-if ($fix > 0 && $played === true && $didNotPlay === false) {
+if ($fix > 0 && $gamePlayed === true && $didNotPlay === false) {
     if ($item['multiplier'] === 2 || $item['multiplier'] === 3) {
         echo '<b><p class="p">'.$item1['stats']['total_points'] * $item['multiplier'].'</p></b>';
     } else {
@@ -32,18 +36,18 @@ if ($fix > 0 && $played === true && $didNotPlay === false) {
     }
 }
 echo '</div>';
-if ($fix === 1 && $notPlayed === true) {
+if ($fix === 1 && $gameNotPlayed === true) {
     if ($stp === 1) {
         echo '<span class="stp">Still to play</span>';
     } elseif ($stp > 1) {
         echo '<span class="stp">'. $stp .' Still to play</span>';
     }
-} elseif ($fix === 1 && $didNotPlay === true) {
+} elseif ($fix === 1 && $didNotPlay === true && $didPlay === false) {
     echo '<span class="stp">Did not play</span>';
 }
-if ($fix === 2 && $notPlayed === true) {
+if ($fix === 2 && $gameNotPlayed === true) {
     echo '<span class="stp">'. $stp .' Still to play</span>';
-} elseif ($fix === 2 && $didNotPlay === true) {
+} elseif ($fix === 2 && $didNotPlay === true && $didPlay === false) {
     echo '<span class="stp">Did not play</span>';
 }
 ?>
