6b8f91af08e5bc5cd9fe6662dfab770a2b5d1161
Sean Kirk <seankirk757@gmail.com>
2023-07-17 22:39:51 +0100

    Updated user and league ID's

--- php/dates.php
+++ php/dates.php
@@ -3,11 +3,11 @@ date_default_timezone_set('Europe/London');
 $date = date("Y-m-d H:i:s");
 
 $data = json_decode(file_get_contents("https://fantasy.premierleague.com/api/bootstrap-static/"), true);
-$leagues = json_decode(file_get_contents("https://fantasy.premierleague.com/api/entry/329312/"), true);
-$navStandings = json_decode(file_get_contents("https://fantasy.premierleague.com/api/leagues-classic/66174/standings/?page_new_entries=1&page_standings=1&phase=1"), true);
-$tasStandings = json_decode(file_get_contents("https://fantasy.premierleague.com/api/leagues-classic/66778/standings/?page_new_entries=1&page_standings=1&phase=1"), true);
+$leagues = json_decode(file_get_contents("https://fantasy.premierleague.com/api/entry/56467/"), true);
+$navStandings = json_decode(file_get_contents("https://fantasy.premierleague.com/api/leagues-classic/61937/standings/?page_new_entries=1&page_standings=1&phase=1"), true);
+$tasStandings = json_decode(file_get_contents("https://fantasy.premierleague.com/api/leagues-classic/381282/standings/?page_new_entries=1&page_standings=1&phase=1"), true);
 
-$picks = json_decode(file_get_contents("https://fantasy.premierleague.com/api/entry/329312/event/".$leagues['current_event']."/picks/"), true);
+$picks = json_decode(file_get_contents("https://fantasy.premierleague.com/api/entry/56467/event/".$leagues['current_event']."/picks/"), true);
 $live = json_decode(file_get_contents("https://fantasy.premierleague.com/api/event/".$leagues['current_event']."/live/"), true);
 
 $nextgw = $leagues['current_event']+1;
