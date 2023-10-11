9677146234a1de8b68d47a705930622511f56670
Sean Kirk <seankirk757@gmail.com>
2023-10-11 19:04:17 +0100

    GW8

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -5,7 +5,7 @@ import urllib.request
 ################################ Data mining from the FPL API ################################
 # The only 2 parameters to specify are "teamID" and "lastGameweek"
 teamID = 56467
-lastGameweek = 6
+lastGameweek = 8
 
 # Get the detailed info about a given FPL Manager’s Team and a given game week
 gameweekData = {}
@@ -243,4 +243,4 @@ ax6.legend(wedges, positions,
 
 ax6.set_xlabel("Points per position over the season")
 
-fig.savefig('gw1-9/gw6.png')
+fig.savefig('gw1-9/gw8.png')
